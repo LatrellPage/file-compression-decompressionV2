bc3e4681421316109e77a2c6c822eee9043c5281
Latrell Page <pagelatrell14@gmail.com>
2024-07-01 19:12:36 -0400

    Created FrontendCOntroller class for handling react app, copy react static to resources folder, configuired pox.xml, appliation.properities, and system properites for correct version and deployment

---
+++ src/main/resources/static/precache-manifest.fd295d618f0ca889decb6bcada74f1d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbbbd018d2c0dfcf5b10974b53d64466",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d3017d44d30b49afde0",
+    "url": "/static/css/main.4c19f2c2.chunk.css"
+  },
+  {
+    "revision": "93ac15dfe03a1e9fbfbb",
+    "url": "/static/js/2.1aaf129f.chunk.js"
+  },
+  {
+    "revision": "268cbd87c1870a295ec8",
+    "url": "/static/js/3.b8a84e90.chunk.js"
+  },
+  {
+    "revision": "8d3017d44d30b49afde0",
+    "url": "/static/js/main.0743d4c8.chunk.js"
+  },
+  {
+    "revision": "38886b36738cfd157afc",
+    "url": "/static/js/runtime~main.7f23aee2.js"
+  }
+]);
